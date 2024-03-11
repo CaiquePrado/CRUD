dfe1186d0d503af3d1a7911f6c75c2a7d3587951
CaiquePrado <caiqueprado76@gmail.com>
2024-03-11 15:40:19 -0300

    feat: add create person service

---
+++ src/main/java/br/com/crud/domain/person/services/usecases/impl/CreatePersonUseCaseImpl.java
@@ -0,0 +1,47 @@
+package br.com.crud.domain.person.services.usecases.impl;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.crud.domain.address.entity.Address;
+import br.com.crud.domain.address.repository.AddressRepository;
+import br.com.crud.domain.person.entity.Person;
+import br.com.crud.domain.person.repository.PersonRepository;
+import br.com.crud.domain.person.services.usecases.CreatePersonUseCase;
+
+@Service
+public class CreatePersonUseCaseImpl implements CreatePersonUseCase {
+
+  @Autowired
+  private PersonRepository personRepository;
+
+  @Autowired 
+  AddressRepository addressRepository;
+
+  @Override
+  public Person execute(Person person) {
+
+    Person savedPerson = savePerson(person);
+
+    saveAddresses(person.getAddresses(), savedPerson);
+
+    return savedPerson;
+  }
+
+  private Person savePerson(Person person) {
+    return personRepository.save(person);
+  }
+
+  private void saveAddresses(List<Address> addresses, Person savedPerson) {
+    if (addresses != null) {
+      addresses = addresses.stream().map(address ->{
+        address.setPerson(savedPerson);
+        return addressRepository.save(address);
+      }).collect(Collectors.toList());
+    }
+  }
+}
+
