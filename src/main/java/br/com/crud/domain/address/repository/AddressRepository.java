2d50bff6024b27ef6b33bc30429fb02bfcbbcf49
CaiquePrado <caiqueprado76@gmail.com>
2024-03-12 09:50:16 -0300

    feat: add findByPerson method to AddressRepository

--- src/main/java/br/com/crud/domain/address/repository/AddressRepository.java
+++ src/main/java/br/com/crud/domain/address/repository/AddressRepository.java
@@ -1,13 +1,17 @@
 package br.com.crud.domain.address.repository;
 
 import java.util.UUID;
+import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import br.com.crud.domain.address.entity.Address;
+import br.com.crud.domain.person.entity.Person;
 
 @Repository
 public interface AddressRepository extends JpaRepository<Address, UUID> {
   
+  List<Address> findByPerson(Person person);
+
 }
