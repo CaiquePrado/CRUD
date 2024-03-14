23266865a908b10617041b90005607719e909572
CaiquePrado <caiqueprado76@gmail.com>
2024-03-14 09:04:33 -0300

    refactor: refactoring swagger

--- src/main/java/br/com/crud/CrudApplication.java
+++ src/main/java/br/com/crud/CrudApplication.java
@@ -3,11 +3,7 @@ package br.com.crud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.info.Info;
-
 @SpringBootApplication
-@OpenAPIDefinition(info = @Info(title = "Crud Persons",description = "API Person address",version = "1"))
 public class CrudApplication {
 
 	public static void main(String[] args) {
